b1246fa2d61da1e61579097f2ffd85ced309f41b
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-01-03 12:20:57 +0530

    Update install.bash

--- install.bash
+++ install.bash
@@ -10,39 +10,35 @@ cp -r $dir/etc/checks /etc/cwput
 #cwput
 
 ports=$(cat Service_List|grep PORTS|awk -F '=' '{print$2}'|sed 's/,/ /g')
-#apps=$(cat Service_List|grep SERVICE|awk -F '=' '{print$2}'|sed 's/,/ /g')
+apps=$(cat Service_List|grep SERVICE|awk -F '=' '{print$2}'|sed 's/,/ /g')
 url=$(cat Service_List|grep URL|awk -F '=' '{print$2}'|sed 's/,/ /g')
 
-for num in ${ports[@]}
-do 
-cp port /etc/cwput/checks/port_"$num"
-sed -i "s/PORTNUMBER/$num/g" /etc/cwput/checks/port_"$num"
-chmod ugo+x /etc/cwput/checks/port_"$num"
-done
+  for num in ${ports[@]}
+    do 
+    cp port /etc/cwput/checks/port_"$num"
+    sed -i "s/PORTNUMBER/$num/g" /etc/cwput/checks/port_"$num"
+    chmod ugo+x /etc/cwput/checks/port_"$num"
+    done
 
-for toll in ${apps[@]}
-do
-name=$(echo $toll|awk -F ':' '{print$1}')
-jar=$(echo $toll|awk -F ':' '{print$2}')
-cp service /etc/cwput/checks/$name
-sed -i "s#SERVICENAME#"$jar"#g" /etc/cwput/checks/$name
+  for toll in ${apps[@]}
+    do
+    name=$(echo $toll|awk -F ':' '{print$1}')
+    jar=$(echo $toll|awk -F ':' '{print$2}')
+    cp service /etc/cwput/checks/$name
+    sed -i 's/METRICNAME/$name/g' /etc/cwput/checks/$name
+    sed -i "s#SERVICENAME#"$jar"#g" /etc/cwput/checks/$name
+    chmod ugo+x /etc/cwput/checks/$name
+    done
 
-#echo -e "$toll is here"
-#chmod ugo+x /etc/cwput/checks/$toll
-done
-#echo -e "$ports - $apps"
-
-
-for link in ${url[@]}
-do
-temp=$(echo $link|sed 's/\./_/g')
-cp url /etc/cwput/checks/url_"$temp"
-sed -i "s/URLNAME/$link/g" /etc/cwput/checks/url_"$temp"
-done
-
+  for link in ${url[@]}
+    do
+    temp=$(echo $link|sed 's/\./_/g')
+    cp url /etc/cwput/checks/url_"$temp"
+    sed -i "s/URLNAME/$link/g" /etc/cwput/checks/url_"$temp"
+    done
 
 grep -l '/bin/cwput' /var/spool/cron/crontabs/root
-if [ $? -ne 0 ]
-then
-echo "*/5 * * * * /bin/cwput"  >> /var/spool/cron/root
-fi
+  if [ $? -ne 0 ]
+    then
+    echo "*/5 * * * * /bin/cwput"  >> /var/spool/cron/root
+  fi
